64f6572a2fbb5611b9e18ed4ce850866dbf89cf5
Nati Gurevich <nati_g4@hotmail.com>
2022-01-18 16:20:32 +0200

    change to nice func

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,8 +1,8 @@
-export function RootCmp() {
+export const RootCmp = () => {
   return (
     <div className='App'>
       <h1>We Start Here</h1>
       <h1>Nati G made a change</h1>
     </div>
   );
-}
+};
