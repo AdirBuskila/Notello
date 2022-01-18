688b600a19e466671b3b33d733225471b3a1c88d
NetanelCohen <ncforb@gmail.com>
2022-01-18 19:58:38 +0200

    nc working on services 2

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,8 +1,7 @@
 export const utilService = {
     makeId,
     makeLorem,
-    getRandomIntInclusive,
-    delay
+    getRandomIntInclusive
 }
 
 function makeId(length = 6) {
@@ -23,4 +22,11 @@ function makeLorem(size = 100) {
         size--;
         txt += words[Math.floor(Math.random() * words.length)] + ' ';
     }
+    return txt;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
