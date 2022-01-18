ac452a760778696667c7d68690abb2acd2c31915
NetanelCohen <ncforb@gmail.com>
2022-01-18 20:04:07 +0200

    nc working on services

--- src/services/async-storage.service.js
+++ src/services/async-storage.service.js
@@ -15,52 +15,37 @@ function query(entityType) {
 }
 
 
-function get(entityType, entityId) {
-    return query(entityType)
-        .then(entities => entities.find(entity => entity._id === entityId))
+async function get(entityType, entityId) {
+    let entities = await query(entityType);
+    const entity = entities.find(entity => entity._id === entityId);
+    return entity
 }
-function post(entityType, newEntity) {
+
+async function post(entityType, newEntity) {
     newEntity._id = utilService.makeId()
-    return query(entityType)
-        .then(entities => {
-            entities.push(newEntity)
-            _save(entityType, entities)
-            return newEntity
-        })
+    let entities = await query(entityType);
+    entities.push(newEntity)
+    _save(entityType, entities)
+    return newEntity
 }
 
-
-
-function put(entityType, updatedEntity) {
-    return query(entityType)
-        .then(entities => {
-            const idx = entities.findIndex(entity => entity._id === updatedEntity._id)
-            entities.splice(idx, 1, updatedEntity)
-            _save(entityType, entities)
-            return updatedEntity
-        })
+async function put(entityType, updatedEntity) {
+    let entities = await query(entityType);
+    const idx = entities.findIndex(entity => entity._id === updatedEntity._id)
+    entities.splice(idx, 1, updatedEntity)
+    _save(entityType, entities)
+    return updatedEntity
 }
 
 function remove(entityType, entityId) {
-    return query(entityType)
-        .then(entities => {
-            const idx = entities.findIndex(entity => entity._id === entityId)
-            entities.splice(idx, 1)
-            _save(entityType, entities)
-        })
+    let entities = await query(entityType);
+    const idx = entities.findIndex(entity => entity._id === entityId)
+    entities.splice(idx, 1)
+    _save(entityType, entities)
 }
 
 
 function _save(entityType, entities) {
     console.log('entityType FROM SAVE!', entityType)
     localStorage.setItem(entityType, JSON.stringify(entities))
-}
-
-function _makeId(length = 5) {
-    var text = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return text
 }
