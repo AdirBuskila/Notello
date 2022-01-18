72d2614dd467628d05e961e86bf33ca2809584c4
Adir Buskila <Adirbu98@gmail.com>
2022-01-18 19:13:38 +0200

    adding content to home page

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -3,9 +3,13 @@ import hero from '../assets/img/hero.png'
 export const HomePage = () => {
   return (
     <div className='home-container flex column align-center'>
-      Welcome To The Home Page
+      <h3> Welcome To The Home Page</h3>
       <div className="hero-container">
       <img className='hero-img' src={hero} / >
+      </div>
+      <div className="home-txt">
+      <h1>Notello helps teams move work forward.</h1>
+        <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Notello.</p>
       </div>
     </div>
   );
