4e3e28c9934ebaf0549e37bc13d11c07deefa591
Nati Gurevich <nati_g4@hotmail.com>
2022-01-19 00:34:54 +0200

    styling for Nati C

--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -4,7 +4,7 @@ import { Card } from './UI/Card';
 export const GroupList = ({ groups }) => {
   if (!groups.length) return <q>No groups</q>;
   return (
-    <Card className='groups-container flex column'>
+    <Card className='groups-container flex'>
       {groups.map((group) => (
         <GroupPreview key={group._id} group={group} />
       ))}
