1904ef73d8dad462c98b08ffd9c98e058296995c
Nati Gurevich <nati_g4@hotmail.com>
2022-01-18 19:12:53 +0200

    move to sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './assets/css/main.css';
+import './assets/styles/styles.scss';
 import { RootCmp } from './root-cmp';
 
 ReactDOM.render(
