93772238bbf6f82a9320f7572aab2ae1fc13fd2a
NetanelCohen <ncforb@gmail.com>
2022-01-18 21:28:04 +0200

    header changed 2

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -8,7 +8,7 @@ export const AppHeader = () => {
       <header className='flex space-between align-center'>
         <NavLink to='/' className='flex'>
           <img className='logo-img' src={logo} />
-          <div className='logo'> Notello </div>
+          <div className='logo'> Notellox </div>
         </NavLink>
         <nav className='nav-links clean-list flex align-center'>
           <NavLink to='/board'>Board</NavLink>
