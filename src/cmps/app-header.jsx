ca509a11c24cd78ed763fc1fc078978ccae81466
Nati Gurevich <nati_g4@hotmail.com>
2022-01-18 19:19:16 +0200

    sass implementation

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,20 +1,20 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/img/notello-logo-static.png'
+import logo from '../assets/img/notello-logo-static.png';
 
 export const AppHeader = () => {
   return (
     <section className='app-header'>
-      <header className=' flex space-between align-center'>
+      <header className='flex space-around align-center'>
         <NavLink to='/'>
           <img className='logo-img' src={logo} />
-          <div className='logo'> Notello </div>{' '}
+          <div className='logo'> Notello </div>
         </NavLink>
         <nav className='nav-links clean-list flex align-center'>
-          <NavLink to='/board'>Board</NavLink> |{' '}
-          {/* <NavLink to='/toy'>MisterToy</NavLink>| */}{' '}
-          {/* <NavLink to='/about'>About</NavLink> */}{' '}
-        </nav>{' '}
-      </header>{' '}
+          <NavLink to='/board'>Board</NavLink>
+          {/* <NavLink to='/toy'>MisterToy</NavLink>| */}
+          {/* <NavLink to='/about'>About</NavLink> */}
+        </nav>
+      </header>
     </section>
   );
 };
