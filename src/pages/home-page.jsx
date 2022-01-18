dbec542600e61467a7871a9b6c318ed6b8ad64d3
Adir Buskila <Adirbu98@gmail.com>
2022-01-18 17:33:23 +0200

    adding hero to home page style

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,7 +1,13 @@
+import hero from '../assets/img/hero.png'
+
 export const HomePage = () => {
   return (
     <div className='home-container flex column align-center'>
       Welcome To The Home Page
+      <div className="hero-container">
+      <img className='hero-img' src={hero} / >
+      </div>
     </div>
   );
 };
+    
