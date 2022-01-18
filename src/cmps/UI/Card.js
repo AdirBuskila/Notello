b457f192d2cab3688e5fbf3f9666eca809b508c2
Nati Gurevich <nati_g4@hotmail.com>
2022-01-19 00:05:55 +0200

    styling and containers

--- src/cmps/UI/Card.js
+++ src/cmps/UI/Card.js
@@ -1,4 +1,4 @@
 export const Card = ({ children, className }) => {
-    const classes = 'card ' + className;
-    return <div className = { classes } > { children } < /div>;
+  const classes = 'card ' + className;
+  return <div className={classes}> {children} </div>;
 };
--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -1,10 +1,13 @@
-import {GroupPreview} from './group-preview'
+import { GroupPreview } from './group-preview';
+import { Card } from './UI/Card';
 
-export const GroupList = ({groups}) => {
-    if (!groups.length) return ( <q>No groups</q> )
-    return (
-        <section className='groups-container'>
-        {groups.map(group => <GroupPreview key={group._id} group={group} /> )}
-            </section>
-    )
-}
+export const GroupList = ({ groups }) => {
+  if (!groups.length) return <q>No groups</q>;
+  return (
+    <Card className='groups-container flex column'>
+      {groups.map((group) => (
+        <GroupPreview key={group._id} group={group} />
+      ))}
+    </Card>
+  );
+};
