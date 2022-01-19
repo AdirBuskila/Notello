0da55e8f639c91201e181b4343940f413c79d711
Nati Gurevich <nati_g4@hotmail.com>
2022-01-20 00:14:46 +0200

    convert to func from class

---
+++ src/pages/board-details class.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Card } from '../cmps/UI/Card';
+import { connect } from 'react-redux';
+
+import { loadBoard } from '../store/actions/board.action';
+
+import { GroupList } from '../cmps/group-list.jsx';
+
+class _BoardDetails extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    this.onLoadBoard();
+  }
+
+  onLoadBoard = async () => {
+    try {
+      const { id } = this.props.match.params;
+      await this.props.loadBoard(id);
+    } catch (err) {
+      console.log('Cant load current board');
+      throw new Error(err);
+    }
+  };
+
+  render() {
+    const { board } = this.props;
+    if (!board || board.length === 0) return <q>Loading...</q>;
+    return (
+      <Card className='board-details-container flex column '>
+        Welcome To The Board Details Page
+        <GroupList onLoadBoard={this.onLoadBoard} groups={board.groups} />
+      </Card>
+    );
+  }
+}
+
+function mapStateToProps({ boardModule }) {
+  return {
+    board: boardModule.board,
+  };
+}
+
+const mapDispatchToProps = {
+  loadBoard,
+};
+
+export const BoardDetails = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_BoardDetails);
