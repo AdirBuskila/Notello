f6838f956678a42dce1100e9bc0426a0f34bf6c2
Nati Gurevich <nati_g4@hotmail.com>
2022-01-19 10:20:06 +0200

    fix base font and color

--- src/cmps/home-header.jsx
+++ src/cmps/home-header.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/img/notello-static-3.gif';
 
 export const HomeHeader = () => {
   return (
     <section className='home-header'>
       <header className='flex space-between align-center'>
-        <NavLink to='/' className='flex logo-img'>
-          {/* <img className='logo-img' src={logo} /> */}
-          {/* <div className='logo'> Notello </div> */}
-        </NavLink>
+        <NavLink to='/' className='flex logo-img'></NavLink>
         <nav className='nav-links clean-list flex align-center'>
           <NavLink to='/board'>Board</NavLink>
           <NavLink to='/login'>Login</NavLink>
