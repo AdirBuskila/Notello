8d83a174c59569452711ca294c40ecf4edf26ea4
Adir Buskila <Adirbu98@gmail.com>
2022-01-22 01:11:23 +0200

    styling app header

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import BoardsMenu from './boards-menu';
 
 export const AppHeader = () => {
   return (
-    <section className='app-header'>
-      <header className='flex space-between align-center'>
+      <header className='app-header flex align-center'>
         <NavLink to='/' className='flex logo-img'></NavLink>
+        <BoardsMenu/>
         <nav className='nav-links clean-list flex align-center'>
-          <NavLink to='/board'>Board</NavLink>
         </nav>
       </header>
-    </section>
   );
 };
