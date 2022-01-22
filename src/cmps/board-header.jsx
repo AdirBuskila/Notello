a903f93f7b7207df086d6d0170acdeaba4f154ec
Nati Gurevich <nati_g4@hotmail.com>
2022-01-22 08:09:02 +0200

    testing git

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -3,26 +3,23 @@ import Rating from '@mui/material/Rating';
 
 import { boardService } from '../services/board.service';
 
-import DOWNICON from '../assets/img/down-arrow.png'
+import DOWNICON from '../assets/img/down-arrow.png';
 
 export const BoardHeader = (props) => {
-
-  const [currBoard, setCurrBoard] = useState(props.board)
+  const [currBoard, setCurrBoard] = useState(props.board);
 
   const onHandleChange = ({ target }) => {
     const value = target.value;
-
-  }
-
-
-  if (!currBoard) return (<h1> No board </h1>)
+  };
+  ///
+  if (!currBoard) return <h1> No board </h1>;
   return (
     <section className='flex'>
       <div>
         {/* Board <span><img src={DOWNICON} alt="down-icon" /></span> */}
       </div>
       <input defaultValue={currBoard.title} onChange={onHandleChange} />
-      <Rating name="half-rating" defaultValue={0} precision={1} max={1} />
+      <Rating name='half-rating' defaultValue={0} precision={1} max={1} />
     </section>
-  )
+  );
 };
