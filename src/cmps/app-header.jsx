8d5b89f7d4202fdef4cd1079ded148ef55f73cef
Nati Gurevich <nati_g4@hotmail.com>
2022-01-24 02:25:56 +0200

    styling to Icons badges and board list

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import BoardsMenu from './boards-menu';
-import ArrowDown from '../assets/img/white-arrow-down.png';
 import { WorkspacesHeaderModal } from '../cmps/workspaces-header-modal';
 import { RecentHeaderModal } from '../cmps/recent-header-modal';
 import { StarredHeaderModal } from './starred-header-modal';
@@ -11,9 +9,7 @@ import { CreateHeaderModal } from './create-header-modal';
 export const AppHeader = () => {
   return (
     <header className='app-header flex align-center'>
-      <div className='logo-container flex align-center '>
-        <NavLink to='/' className='logo-img brdr-rds'></NavLink>
-      </div>
+      <NavLink to='/' className='logo-img brdr-rds'></NavLink>
       <WorkspacesHeaderModal />
       <RecentHeaderModal />
       <StarredHeaderModal />
