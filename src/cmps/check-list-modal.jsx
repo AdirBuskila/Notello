06225bee1f7ef507d71a0bc4b08155b2c1f90dbe
Nati Gurevich <nati_g4@hotmail.com>
2022-01-27 16:07:55 +0200

    modal styling

--- src/cmps/check-list-modal.jsx
+++ src/cmps/check-list-modal.jsx
@@ -6,14 +6,14 @@ import Button from '@mui/material/Button';
 import CheckBoxOutlined from '@mui/icons-material/CheckBoxOutlined';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
-import {utilService} from '../services/util.service'
+import { utilService } from '../services/util.service';
 
 export const CheckListModal = (props) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
-  const {board, groupIdx, taskIdx, task, isCheckListAcctivated} = props;
-  const dispatch = useDispatch()
+  const { board, groupIdx, taskIdx, task, isCheckListAcctivated } = props;
+  const dispatch = useDispatch();
   const [checklistName, setChecklistName] = useState('');
 
   const handleClick = (event) => {
@@ -30,16 +30,20 @@ export const CheckListModal = (props) => {
   };
 
   const onAddClick = () => {
-    if (!checklistName) return // add nice modal
-    const checklist = {_id: utilService.makeId(), title: checklistName, todos: []}
+    if (!checklistName) return; // add nice modal
+    const checklist = {
+      _id: utilService.makeId(),
+      title: checklistName,
+      todos: [],
+    };
     task.checklists.unshift(checklist);
     board.groups[groupIdx].tasks[taskIdx] = task;
-    const action = {type: 'SET_BOARD', board};
+    const action = { type: 'SET_BOARD', board };
     dispatch(action);
     props.setIsCheckListAcctivated(!isCheckListAcctivated);
-    setChecklistName('')
-    handleClose()
-  }
+    setChecklistName('');
+    handleClose();
+  };
 
   return (
     <div className='button-container flex'>
@@ -56,28 +60,41 @@ export const CheckListModal = (props) => {
           vertical: 'bottom',
           horizontal: 'left',
         }}>
-        <div sx={{ p: 0.5, width: '304px', height: '227px' }}>
-          <div className='check-list-modal flex justify-center'>
-            Add checklist
-            <a onClick={handleClose}>
+        <React.Fragment>
+          <div className='check-list-modal flex column align center'>
+            <div className='close-button pointer' onClick={handleClose}>
               ✕
-            </a>
-          </div>
-          <div className='check-list-inner flex column'>
-            <div className='check-list-title flex column '>
-              <span>Title</span>
-              <input onChange={onHandleName} placeholder='Checklist'></input>
-              <span>Copy items from</span>
-              <select>
-                <option value='-1'>none</option>
-                <option value='0'>checklist-1</option>
-                <option value='1'>checklist-2</option>
-                <option value='2'>checklist-3</option>
-              </select>
             </div>
-            <button onClick={onAddClick}>Add</button>
+
+            <div className='check-list-inner flex column align-center'>
+              <p>Add checklist</p>
+              <div className='check-list-title flex column '>
+                <div className='flex column'>
+                  <span>Title</span>
+                  <input
+                    onClick={(ev) => ev.stopPropagation()}
+                    defaultValue='Checklist'
+                    autoFocus
+                    onChange={onHandleName}
+                    placeholder='Checklist'
+                    onFocus={(ev) => {
+                      ev.currentTarget.select();
+                    }}></input>
+                </div>
+                <div className='flex column'>
+                  <span>Copy items from</span>
+                  <select>
+                    <option value='-1'>(none)</option>
+                    <option value='0'>checklist-1</option>
+                    <option value='1'>checklist-2</option>
+                    <option value='2'>checklist-3</option>
+                  </select>
+                </div>
+              </div>
+              <button onClick={onAddClick}>Add</button>
+            </div>
           </div>
-        </div>
+        </React.Fragment>
       </Popover>
     </div>
   );
