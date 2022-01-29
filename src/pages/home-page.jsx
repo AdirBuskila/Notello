f0e9a9a02af4b79656197cccc16269001cfef5c7
Adir Buskila <Adirbu98@gmail.com>
2022-01-29 14:31:14 +0200

    home page

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -50,14 +50,14 @@ export class HomePage extends React.Component {
         </section>
         <div className='hero-container flex align-center'>
           <img className='hero-img' src={hero} />
-          <div className='home-txt flex column'>
+          <div className='home-txt'>
             <h1>Notello helps teams move work forward.</h1>
             <p>
               Collaborate, manage projects, and reach new productivity peaks.
               From high rises to the home office, the way your team works is
               unique—accomplish it all with Notello.
             </p>
-            <Link className='home-link flex align-center' to='/board'>
+            <Link className='home-link ' to='/board'>
               Get Started!
             </Link>
           </div>
