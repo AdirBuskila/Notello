7e6d8f1cccb99c53ba8c205de8425598c696086b
Adir Buskila <Adirbu98@gmail.com>
2022-01-29 23:47:28 +0200

    fixing by member in comments section

--- src/cmps/task-details-cmps/comments-section.jsx
+++ src/cmps/task-details-cmps/comments-section.jsx
@@ -34,7 +34,7 @@ export const CommentsSection = (props) => {
                 src={comment.byMember.imgUrl}
                 alt={comment.byMember.fullname}
                 sx={{
-                  bgcolor: 'lightblue',
+                  bgcolor: utilService.stringToColor(comment.byMember.fullname),
                   width: 32,
                   height: 32,
                   marginInlineEnd: 1,
