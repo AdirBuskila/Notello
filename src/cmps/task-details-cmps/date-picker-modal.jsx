d210973ca697d0c1d1ff52df14d4bab52166999b
Adir Buskila <Adirbu98@gmail.com>
2022-01-29 20:14:04 +0200

    fix

--- src/cmps/task-details-cmps/date-picker-modal.jsx
+++ src/cmps/task-details-cmps/date-picker-modal.jsx
@@ -25,7 +25,6 @@ export function DatePickerModal(props) {
 
   const handleDateChange = (selectedDate) => {
     setSelectedDate(selectedDate);
-    console.log('selectedDate', selectedDate);
     const newDueDate = { date: selectedDate, isDone: false };
     const newArray = [];
     newArray.push(newDueDate);
